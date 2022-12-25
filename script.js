6ef1821898ec83781a079a073464b67bdbe97936
Shiva <2230@tkmce.ac.in>
2022-12-25 18:23:42 +0530

    added change background color feature on click

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 alert("This is a javascript tag")
+function setBackground() {
+    let mybody=document.getElementsByTagName("body")[0];
+    //mybody.style.background-color="red";
+    mybody.style.backgroundColor = "red";
+    
+}
